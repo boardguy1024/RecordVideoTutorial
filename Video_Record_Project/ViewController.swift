f8676b84261bf433c2d83975ffbceb7c7e85dd2a
park kyung suk <boardguy1024@gmail.com>
2019-06-16 17:33:46 +0900

    first commit

---
+++ Video_Record_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video_Record_Project
+//
+//  Created by park kyung suk on 2019/06/16.
+//  Copyright © 2019 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
